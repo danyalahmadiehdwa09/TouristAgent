a43455c4937b505533b58a182d77add419cfa7dc
danyalahmadiehdwa09 <98920647+danyalahmadiehdwa09@users.noreply.github.com>
2022-03-08 23:19:40 +0200

    Login_Phase1

--- TouristAgent2/app/src/main/java/com/example/touristagent/LoginFragment.java
+++ TouristAgent2/app/src/main/java/com/example/touristagent/LoginFragment.java
@@ -4,8 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+
+import java.io.IOException;
+
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class LoginFragment extends Fragment {
 
@@ -14,12 +24,42 @@ public class LoginFragment extends Fragment {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view =  inflater.inflate(R.layout.fragment_login, container, false);
+        Button btn = (Button) view.findViewById(R.id.btn_register);
+        TextView username = view.findViewById(R.id.et_name);
+        TextView pass = view.findViewById(R.id.et_password);
+        TextView error = view.findViewById(R.id.et_error);
+        btn.setOnClickListener(v -> {
+            OkHttpClient client = new OkHttpClient();
+
+            RequestBody requestBody = new MultipartBody.Builder()
+                        .setType(MultipartBody.FORM)
+                        .addFormDataPart("username", String.valueOf(username))
+                        .addFormDataPart("password", String.valueOf(pass))
+                        .build();
+
+                Request request = new Request.Builder()
+                        .url("http:127.0.0.1:5000/")
+                        .post(requestBody)
+                        .build();
+
+            try (Response response = client.newCall(request).execute()) {
+                if (!response.isSuccessful()) try {
+                    throw new IOException("Unexpected code " + response);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        });
+        return view;
     }
 
 }
--- TouristAgent2/app/src/main/java/com/example/touristagent/RegisterFragment.java
+++ TouristAgent2/app/src/main/java/com/example/touristagent/RegisterFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -14,12 +16,30 @@ public class RegisterFragment extends Fragment {
         // Required empty public constructor
     }
 
+    public void savetoDatabase(TextView name, TextView email, TextView pass) {
+
+
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_register, container, false);
+        Button btn = (Button) view.findViewById(R.id.btn_register);
+        TextView email = view.findViewById(R.id.et_email);
+        TextView pass = view.findViewById(R.id.et_firstname);
+        TextView username = view.findViewById(R.id.et_email);
+        TextView pass2 = view.findViewById(R.id.et_repassword);
+        TextView error = view.findViewById(R.id.et_error);
+        btn.setOnClickListener(v -> {
+            if (pass2 != pass)
+            { error.setText("passwords are not matching"); }
+            else {
+                savetoDatabase(username,email,pass);
+            }
+    });
+                return view;
 
-}
+    }}
