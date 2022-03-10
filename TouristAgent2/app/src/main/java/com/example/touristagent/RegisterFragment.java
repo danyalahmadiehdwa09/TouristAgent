2cbb578f18dbbdd55012907568671463717f3ed7
Danyalahmadiehdwa09 <dwa09@mail.aub.edu>
2022-03-10 22:24:05 +0200

    Revert "Login_Phase1"
    This reverts commit a43455c4937b505533b58a182d77add419cfa7dc.

--- TouristAgent2/app/src/main/java/com/example/touristagent/RegisterFragment.java
+++ TouristAgent2/app/src/main/java/com/example/touristagent/RegisterFragment.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -16,30 +14,12 @@ public class RegisterFragment extends Fragment {
         // Required empty public constructor
     }
 
-    public void savetoDatabase(TextView name, TextView email, TextView pass) {
-
-
-
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_register, container, false);
-        Button btn = (Button) view.findViewById(R.id.btn_register);
-        TextView email = view.findViewById(R.id.et_email);
-        TextView pass = view.findViewById(R.id.et_firstname);
-        TextView username = view.findViewById(R.id.et_email);
-        TextView pass2 = view.findViewById(R.id.et_repassword);
-        TextView error = view.findViewById(R.id.et_error);
-        btn.setOnClickListener(v -> {
-            if (pass2 != pass)
-            { error.setText("passwords are not matching"); }
-            else {
-                savetoDatabase(username,email,pass);
-            }
-    });
-                return view;
+        return inflater.inflate(R.layout.fragment_register, container, false);
+    }
 
-    }}
+}
